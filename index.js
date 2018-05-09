8534d38322b6dcd1fb05ea0f30742a51e396ee43
Eddie Melo <heriberto@melo.nyc>
2018-05-09 00:11:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function getCommits(el) {
   const name = el.dataset.repo
   const req = new XMLHttpRequest()
   req.addEventListener("load", displayCommits)
-    request.open("GET", `https://api.github.com/repos/${username.value}/${el.dataset.repository}/commits`)
+  request.open("GET", `https://api.github.com/repos/${username.value}/${el.dataset.repository}/commits`)
   req.send()
 }
 
