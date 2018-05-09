3ccd9e00c7e57fae47b4b6fe529bb376e03564cc
Eddie Melo <heriberto@melo.nyc>
2018-05-09 00:00:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function showRepositories(event, data) {
   //this is set to the XMLHttpRequest object that fired the event
 
   var repos = JSON.parse(this.responseText)
-  let responseList = `<ul>${repos.map(r => '<li><a href="' + r.html_url + '">' + r.name + ' - <a href="#" data-repo="' + r.name + '" onclick="getCommits(this)">Get Commits</a></li>' + ' - <a href="#" data-repo="' + r.name + '" onclick="getBranches(this)">Get Branches</a></li>').join('')}</ul>`
+  let repoList = `<ul>${repos.map(r => '<li><a href="' + r.html_url + '">' + r.name + ' - <a href="#" data-repo="' + r.name + '" onclick="getCommits(this)">Get Commits</a></li>' + ' - <a href="#" data-repo="' + r.name + '" onclick="getBranches(this)">Get Branches</a></li>').join('')}</ul>`
   document.getElementById("repositories").innerHTML = repoList
 }
 
